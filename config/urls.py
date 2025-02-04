87cab15309f67ae2d58f04558775c2c596adff9e
macalistervadim <macalistervadim@yandex.ru>
2025-02-04 17:54:26 +0600

    change config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(
+    urlpatterns += static(  # type: ignore
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
