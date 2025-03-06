728191ffe954e3d573d77dd7e6ada94f9efae545
macalistervadim <macalistervadim@yandex.ru>
2025-03-06 06:10:23 +0600

    feature: add user for context in websocket Consumer

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -2,10 +2,12 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self) -> None:
+        self.user = self.scope["user"]
         self.id = self.scope["url_route"]["kwargs"]["course_id"]
         self.room_group_name = f"chat_{self.id}"
         async_to_sync(self.channel_layer.group_add)(
@@ -28,11 +30,14 @@ class ChatConsumer(WebsocketConsumer):
         if text_data:
             text_data_json = json.loads(text_data)
             message = text_data_json["message"]
+            now = timezone.now()
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
                     "type": "chat_message",
                     "message": message,
+                    "user": self.user.username,
+                    "datetime": now.isoformat(),
                 },
             )
 
